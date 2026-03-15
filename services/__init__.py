c0006d28fd3dbf8e03b84749a84d2af4970f1710
yomo4 <whooshgovno77@gmail.com>
2026-03-16 04:29:26 +0500

    Add files via upload

--- services/__init__.py
+++ services/__init__.py
@@ -1,65 +1,10 @@
 """
 Services модуль для CRUPTOANON
-Включает криптографию, защиту, обфускацию и построение APK
+Криптер с полиморфизмом и каскадным шифрованием
 """
 
-from .apk_crypter import APKCrypter
-from .obfuscator import ObfuscationManager, CodeObfuscator
-from .protection import (
-    ProtectionManager,
-    SecurityToken,
-    IntegrityChecker,
-    RateLimiter,
-    AntiTamperingMonitor,
-    ExecutionEnvironmentValidator,
-    ProtectionException
-)
-from .advanced_protection import (
-    AndroidEnvironmentDetector,
-    AntiAnalysisProtection,
-    ProtectionDecorator,
-    DetectionType
-)
-from .npmanager import (
-    NPManager,
-    NPMPackage,
-    NPManagerFactory,
-    NPManagerBatchProcessor,
-    TechFrameworkMasker,
-    AndroidSystemMasker,
-    MythicalName,
-    SystemServiceMask
-)
-from .full_protector import (
-    FullAPKProtector,
-    APKProtectionPipeline
-)
-from .stub_builder import StubBuilder
+from .apk_packer import APKPacker
 
 __all__ = [
-    "APKCrypter",
-    "ObfuscationManager",
-    "CodeObfuscator",
-    "ProtectionManager",
-    "SecurityToken",
-    "IntegrityChecker",
-    "RateLimiter",
-    "AntiTamperingMonitor",
-    "ExecutionEnvironmentValidator",
-    "ProtectionException",
-    "AndroidEnvironmentDetector",
-    "AntiAnalysisProtection",
-    "ProtectionDecorator",
-    "DetectionType",
-    "NPManager",
-    "NPMPackage",
-    "NPManagerFactory",
-    "NPManagerBatchProcessor",
-    "TechFrameworkMasker",
-    "AndroidSystemMasker",
-    "MythicalName",
-    "SystemServiceMask",
-    "FullAPKProtector",
-    "APKProtectionPipeline",
-    "StubBuilder",
+    "APKPacker",
 ]
